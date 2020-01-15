45ee5695e777544908be50d1a774bba3338cd715
Alex <cookie_swag@icloud.com>
2020-01-15 18:47:03 +0300

    created functions [chooseExpenses], [chooseOptExpenses], [detectDayBudget], [detectLevel], [checkSavings]. Added to appData new object(?) optionalExpenses.

--- moonscript.js
+++ moonscript.js
@@ -1,8 +1,12 @@
-function MoonScript(){
 'use strict';
+function MoonScript(){
 
-let money = +prompt ('What is your budget for this month?', ''),
-    date  = prompt ('Please input date in format YYYY-MM-DD', '');
+let money = +prompt ('What is your budget for this month?', ''); 
+// if (money == null) {return;} fix this line please
+ while (money == '' || isNaN(money)) {
+     money = +prompt ('What is your budget for this month?', '');
+ }
+let date  = prompt ('Please input date in format YYYY-MM-DD', '');
 
 var a,
     i,
@@ -15,18 +19,21 @@ var appData =
     timeData : date, // yyyy - mm - dd
     expenses: {},
     income : [],
-    savings : false
+    savings : true,
+    level : 'ok',
+    optionalExpenses : {}
 };
-// console.log('budget ', appData.budget);
+ console.log('budget ', appData.budget);
 
 
-for (let i = 0; i<2; i++) {
+function chooseExpenses() {
+    for (let i = 0; i<2; i++) {
 let a = prompt("Please input name of obligatory expense (1 item) this month (50 symbols max)", ''),
     b = prompt("How much it will cost you?", '');
     if ( typeof(a) === "string" && typeof(a) != null 
          && a !='' && b !='' && a.length < 50 && typeof(b) != null && isNaN(b) != true) {
              b=+b;
-        console.log('done');
+        console.log("expense recorded to " + a + " and it is " + b + " and it is " + (i+1) + " of 2 total");
         console.log(typeof(b));
         appData.expenses[a] = b;
     }
@@ -34,26 +41,67 @@ let a = prompt("Please input name of obligatory expense (1 item) this month (50 
         i--;
         continue;
     }
+  }
+}
+function chooseOptExpenses() {
+    for (let i = 0; i<3; i++) {
+let a = prompt("Please input name of optional expense (1 item) this month (50 symbols max)", ''),
+    b = prompt("How much it will cost you?", '');
+    if ( typeof(a) === "string" && typeof(a) != null 
+         && a !='' && b !='' && a.length < 50 && typeof(b) != null && isNaN(b) != true) {
+             b=+b;
+        console.log("expense recorded to " + a + " and it is " + b + " and it is " + (i+1) + " of 3 total");
+        console.log(typeof(b));
+        appData.optionalExpenses[a] = b;
+    }
+    else {
+        i--;
+        continue;
+    }
+  }
+
 
 }
+chooseExpenses();
+chooseOptExpenses();
 
 console.log(appData.expenses);
-
-appData.moneyperday = appData.budget / 30;
+function detectDayBudget() {
+appData.moneyperday = +((appData.budget / 30).toFixed(1));
 alert('Your daily budget for this month is ' + appData.moneyperday);
+console.log('type of: ' + typeof(appData.moneyperday));
+}
 
+function detectLevel() {
 if (appData.moneyperday < 100) {
+    appData.level = "low paycheck";
     console.log('low paycheck');
 
 } else if (appData.moneyperday > 100 && appData.moneyperday < 2000) {
+    appData.level = "decent paycheck";
     console.log('decent paycheck');
     
 } else if (appData.moneyperday > 2000) {
-    console.log('low paycheck')
+    appData.level = "big paycheck";
+    console.log('big paycheck');
 }
   else {
-      Console.log('Error >.<')
-}  
-
+      appData.level = "error";
+      console.log('Error >.<');
+}
 console.log(appData);
+}
+detectDayBudget();
+detectLevel();
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Input the amount of the deposit", '');
+        let percent = +prompt("Please input interest rate", '');
+
+        appData.monthIncome = (save/100/12*percent);
+        alert("The deposit will bring you " + appData.monthIncome + " per month");
+    }
+}
+checkSavings();
 }
